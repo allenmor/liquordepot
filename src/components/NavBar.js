7239c93bf76020b7417ca905b156bd804f57be6c
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-19 19:47:40 -0400

    init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import './NavBar.css'
 
 function NavBar() {
   return (
-    <div>
+    <div className='nav-container'>
       <p>Home</p>
       <p>Tequila</p>
+      <p>Whiskey</p>
+      <p>Arak</p>
+      <p>Vodka</p>
+      <p>Mezcal</p>
+      <p>Gin</p>
+      <p>Rum</p>
+      <p>Wine</p>
+      <p>Cognac</p>
     </div>
   )
 }
